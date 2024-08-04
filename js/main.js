769c910fc586bf8800a111a5bf4fb958675f7d4b
Dev Mohamed <mohamedhassanmohamed1@gmail.com>
2024-08-05 02:18:03 +0300

    5/8 update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 (function() {
   let arr = [8,4,6,2,4,3,2,0,6,4];
 
-  console.log(arr);
-  arr.sort();
-  console.log(arr);
-  arr.sort((x,y) => {
-    return y - x;
-  })
-  console.log(arr)
-  
+  let res = arr.find(num => num < 5);
+  console.log(res);
+
+  let res2 = arr.filter(num => num < 5);
+
+  console.log(res2)
 
 
 
